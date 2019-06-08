92779801268e4e4a77d5e408c75428a199ea3a6d
Petra <petrabursic0@gmail.com>
2019-06-08 13:57:14 +0200

    Prijava i registracija
    Uređivanje i navigacija prijave i registracije korisnika.

--- NajamStanova/src/app/app.module.ts
+++ NajamStanova/src/app/app.module.ts
@@ -1,54 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AgmCoreModule } from '@agm/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AgmCoreModule } from "@agm/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { LandingComponent } from './landing/landing.component';
-import { MapComponent } from './map/map.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { FormComponent } from './form/form.component';
-import { ViewComponent } from './view/view.component';
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { LandingComponent } from "./landing/landing.component";
+import { MapComponent } from "./map/map.component";
+import { SidenavComponent } from "./sidenav/sidenav.component";
+import { FormComponent } from "./form/form.component";
+import { ViewComponent } from "./view/view.component";
 
-import { PostService } from './shared/post/post.service'
-import { HttpClientModule } from '@angular/common/http';
+import { PostService } from "./shared/post/post.service";
+import { HttpClientModule } from "@angular/common/http";
 //import {AngularFireModule} from 'angularfire2';
 //import {AngularFireDatabaseModule} from 'angularfire2/database';
-import { environment } from 'src/environments/environment';
-import { PostDetailComponent } from './post-detail/post-detail.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { LoginComponent } from './login/login.component';
+import { environment } from "src/environments/environment";
+import { PostDetailComponent } from "./post-detail/post-detail.component";
+import { RegistrationComponent } from "./registration/registration.component";
+import { LoginComponent } from "./login/login.component";
 
-const routes : Routes = [
+const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: LandingComponent
   },
   {
-    path: 'map',
+    path: "map",
     component: MapComponent
   },
   {
-    path: 'form',
+    path: "form",
     component: FormComponent
   },
   {
-    path: 'view',
+    path: "view",
     component: ViewComponent
   },
   {
-    path: 'registration',
+    path: "registration",
     component: RegistrationComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
   }
 ];
-
 
 @NgModule({
   declarations: [
@@ -76,14 +79,11 @@ const routes : Routes = [
     //AngularFireModule.initializeApp(environment.firebaseConfig),
     //AngularFireDatabaseModule,
     AgmCoreModule.forRoot({
-      apiKey:'AIzaSyBoN1B0apITPX4oS5JM-aBf89jTDpjBfo0'
+      apiKey: "AIzaSyBoN1B0apITPX4oS5JM-aBf89jTDpjBfo0"
     })
   ],
-  entryComponents: [
-    RegistrationComponent,
-    LoginComponent
-  ],
+  entryComponents: [RegistrationComponent, LoginComponent],
   providers: [PostService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
